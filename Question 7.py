c70d9ca8273f583fdb7524fd3c271101e2e3d91b
Caitlin Reid <caitlin.reid@ucalgary.ca>
2026-01-30 17:23:50 -0700

    Complete question 8

--- Question 7.py
+++ Question 7.py
@@ -9,7 +9,7 @@
 
 ##what is invalid input? not numbers? What if number of seconds is greater than the amount in 24 hours?
 
-def secondsSinceMinutesTime(seconds):
+def secondsSinceMidnightTime(seconds):
     hour = seconds//3600
     remainingSecondsMinusHours = seconds%3600
     minutes = remainingSecondsMinusHours//60
@@ -24,5 +24,5 @@ def secondsSinceMinutesTime(seconds):
     return (f"{hour} {minutes} {finalRemainingSeconds} {amOrPM}")
 
 #testing
-print(secondsSinceMinutesTime(82623))
+print(secondsSinceMidnighTime(82623))
 
--- Question 8.py
+++ Question 8.py
@@ -1,6 +1,20 @@
 #Caitlin Reid|30245427   DATA 221 Assignment 1
-# Question 8 (1 Point): Pandas DataFrame with Computed Column
+# Question 8: Pandas DataFrame with Computed Column
 # Install the pandas package and import it using the alias pd. Then:
 # • Create a DataFrame using the provided column data.
 # • Add a new column derived from existing columns.
 # • Print the final DataFrame.
+
+import pandas as pd
+
+data = {
+    "A": [1, 2, 2, 1],
+    "B":[3.1, 4.2, 1.5, 6.3,],
+    "C": [800, 150, 400, 210]
+}
+
+dataFrame = pd.DataFrame(data)
+
+dataFrame["D"] = dataFrame["A"] * dataFrame["B"] + dataFrame["C"]
+
+print (dataFrame)
